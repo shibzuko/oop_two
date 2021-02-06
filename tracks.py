d46b8447c5acfd64a795995469287aed98f4e342
shibzuko <33262442+shibzuko@users.noreply.github.com>
2021-02-07 01:30:31 +0700

    ДЗ 1.2

---
+++ tracks.py
@@ -0,0 +1,30 @@
+class Album:
+    name_album = " "
+    group = " "
+    track_list = " "
+    
+    def __init__(show, get_tracks, add_track, get_duration):
+        show.get_tracks
+        show.add_track
+        show.get_duration
+
+
+class Track(Album):
+    track_name = " "
+    duration = 0
+
+    def __init__(show, track_name_duration)    
+        show.track_name_duration
+
+class album_1 (Album, Track):
+
+class album_2 (Album, Track):
+
+track_1 = album_1()
+
+
+
+
+
+
+
